c347580b08d5093d4d7991ed7e608094f044e72f
Simo Mazil <mohamed.mazil@gmail.com>
2020-01-21 20:08:39 +0100

    add DataTable componenet & model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/DataTable.ts
@@ -0,0 +1,7 @@
+export class DataTable {
+  id: string
+  primaryTitle: string
+  originalTitle: string
+  startYear: string
+  genre: string
+}
