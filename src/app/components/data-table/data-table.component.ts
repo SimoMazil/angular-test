3fa74165de382823eaee7ccc1f37fc6d80cb03db
Simo Mazil <mohamed.mazil@gmail.com>
2020-01-21 21:43:35 +0100

    complete recherche events in data table component

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -7,34 +7,53 @@ import { DataTable } from 'src/app/models/DataTable';
   styleUrls: ['./data-table.component.css']
 })
 export class DataTableComponent implements OnInit {
-  data: DataTable[]
+  data: DataTable[] = [
+    {
+      id: '1',
+      primaryTitle: 'Gone Girl',
+      originalTitle: 'Gone Girl',
+      startYear: '2010',
+      genre: 'Drama'
+    },
+    {
+      id: '2',
+      primaryTitle: 'Fight Club',
+      originalTitle: 'Fight Club',
+      startYear: '1999',
+      genre: 'Action'
+    },
+    {
+      id: '3',
+      primaryTitle: 'Seven',
+      originalTitle: 'Se7en',
+      startYear: '2020',
+      genre: 'Crime'
+    },
+  ]
+
+  lines: DataTable[]
+  filterValue: string = 'title'
 
   constructor() { }
 
   ngOnInit() {
-    this.data = [
-      {
-        id: '1',
-        primaryTitle: 'Primary Title',
-        originalTitle: 'Original Title 1',
-        startYear: 'Start Year 1',
-        genre: 'Genre 1'
-      },
-      {
-        id: '2',
-        primaryTitle: 'Primary Title',
-        originalTitle: 'Original Title 2',
-        startYear: 'Start Year 2',
-        genre: 'Genre 2'
-      },
-      {
-        id: '3',
-        primaryTitle: 'Primary Title',
-        originalTitle: 'Original Title 3',
-        startYear: 'Start Year 3',
-        genre: 'Genre 3'
-      },
-    ]
+    this.lines = this.data.sort((a: any, b: any) => a.startYear - b.startYear)
+  }
+
+  onKey(event: any) {
+    if(event.target.value !== '' && this.filterValue === 'title') {
+      this.lines = this.lines.filter(item => 
+      item.originalTitle.toLowerCase().includes(event.target.value.toLowerCase()) || 
+      item.primaryTitle.toLowerCase().includes(event.target.value.toLowerCase()))
+    } else if(event.target.value !== '' && this.filterValue === 'year') {
+      this.lines = this.lines.filter(item => item.startYear.includes(event.target.value.toString()))
+    } else {
+      this.lines = this.data
+    }
+  }
+
+  onChange(event: any) {
+    this.filterValue = event.target.value;
   }
 
 }
