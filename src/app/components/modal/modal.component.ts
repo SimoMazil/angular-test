b143c5b676b00d79c7ad9f52471a0b5a12f8abf5
Simo Mazil <mohamed.mazil@gmail.com>
2020-01-21 22:20:56 +0100

    add the style for the modal component

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+  title: string
+  genre: string
+  year: string
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.title);
+  }
+
+}
