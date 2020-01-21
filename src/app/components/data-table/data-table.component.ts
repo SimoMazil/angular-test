ce31a5b1bb4f3848bf568dde365db0de3b34195b
Simo Mazil <mohamed.mazil@gmail.com>
2020-01-21 20:29:36 +0100

    add style to the data table component

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataTable } from 'src/app/models/DataTable';
 
 @Component({
   selector: 'app-data-table',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-table.component.css']
 })
 export class DataTableComponent implements OnInit {
+  data: DataTable[]
 
   constructor() { }
 
   ngOnInit() {
+    this.data = [
+      {
+        id: '1',
+        primaryTitle: 'Primary Title',
+        originalTitle: 'Original Title 1',
+        startYear: 'Start Year 1',
+        genre: 'Genre 1'
+      },
+      {
+        id: '2',
+        primaryTitle: 'Primary Title',
+        originalTitle: 'Original Title 2',
+        startYear: 'Start Year 2',
+        genre: 'Genre 2'
+      },
+      {
+        id: '3',
+        primaryTitle: 'Primary Title',
+        originalTitle: 'Original Title 3',
+        startYear: 'Start Year 3',
+        genre: 'Genre 3'
+      },
+    ]
   }
 
 }
