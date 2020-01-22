cdd28afc299327693733229408ad674a7aa01175
Simo Mazil <mohamed.mazil@gmail.com>
2020-01-22 11:57:26 +0100

    add data from the file

---
+++ src/app/models/data.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    id: 'tt0000001',
+    primaryTitle: 'Carmencita',
+    originalTitle: 'Carmencita',
+    startYear: '1894',
+    genre: 'Documentary,Short'
+  },
+  {
+    id: 'tt0000002',
+    primaryTitle: 'Le clown et ses chiens',
+    originalTitle: 'Le clown et ses chiens',
+    startYear: '1892',
+    genre: 'Animation,Short'
+  },
+  {
+    id: 'tt0000003',
+    primaryTitle: 'Pauvre Pierrot',
+    originalTitle: 'Pauvre Pierrot',
+    startYear: '1892',
+    genre: 'Animation,Comedy,Romance'
+  },
+  {
+    id: 'tt0000004',
+    primaryTitle: 'Un bon bock',
+    originalTitle: 'Un bon bock',
+    startYear: '1892',
+    genre: 'Animation,Short'
+  },
+  {
+    id: 'tt0000005',
+    primaryTitle: 'Blacksmith Scene',
+    originalTitle: 'Blacksmith Scene',
+    startYear: '1893',
+    genre: 'Comedy,Short'
+  },
+]
--- src/app/services/data-table.service.ts
+++ src/app/services/data-table.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { data } from '../models/data'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,28 +10,6 @@ export class DataTableService {
   constructor() { }
 
   getData() {
-    return [
-      {
-        id: '1',
-        primaryTitle: 'Gone Girl',
-        originalTitle: 'Gone Girl',
-        startYear: '2010',
-        genre: 'Drama'
-      },
-      {
-        id: '2',
-        primaryTitle: 'Fight Club',
-        originalTitle: 'Fight Club',
-        startYear: '1999',
-        genre: 'Action'
-      },
-      {
-        id: '3',
-        primaryTitle: 'Seven',
-        originalTitle: 'Se7en',
-        startYear: '2020',
-        genre: 'Crime'
-      },
-    ]
+    return data;
   }
 }
